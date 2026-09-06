30ba2700384eeb5a104ac8fbcda322a61c91c92a
Davi <84661631+davijuvino@users.noreply.github.com>
2026-09-06 14:21:00 -0300

    fix: Add GenerationType.IDENTITY and add validations to ApiKey

--- src/main/kotlin/br/com/dindin/domain/model/ApiKey.kt
+++ src/main/kotlin/br/com/dindin/domain/model/ApiKey.kt
@@ -4,10 +4,12 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 
@@ -16,8 +18,8 @@ import jakarta.validation.constraints.NotNull
 data class ApiKey(
 
     @Id
-    @GeneratedValue
-    @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     var id: Long = 0,
 
     @NotNull
@@ -25,7 +27,12 @@ data class ApiKey(
     @JoinColumn(name = "user_id", nullable = false)
     var userId: KomgaUser,
 
+    @NotBlank
+    @Column(name = "pkey", nullable = false, unique = true)
     val pkey: String,
+
+    @NotBlank
+    @Column(name = "comment", nullable = false)
     val comment: String,
 
     ) : Auditable()
