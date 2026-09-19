152ac321b0fc4fdef320616cc42e0445a5f78ce0
Davi <84661631+davijuvino@users.noreply.github.com>
2026-09-19 14:21:27 -0300

    Corrigir typo e padronizar timestamps do Auditable para Instant

--- src/main/kotlin/br/com/dindin/domain/model/Auditable.kt
+++ src/main/kotlin/br/com/dindin/domain/model/Auditable.kt
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
-a
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class Auditable {
